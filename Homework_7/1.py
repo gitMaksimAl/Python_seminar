1cb24a3b77d0a33c4f2bcb3ff2786e3bd7c5d9eb
Maksim Aleksanin <maksim.g00gle@gmail.com>
2023-02-19 19:00:18 +0400

    Homework 7, task 1 - solved

---
+++ Homework_7/1.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+# Винни-Пух считает, что ритм есть, если число слогов
+# (т.е. число гласных букв) в каждой фразе стихотворения одинаковое.
+# Фраза может состоять из одного слова, если во фразе
+# несколько слов, то они разделяются дефисами.
+# Фразы отделяются друг от друга пробелами.
+# Стихотворение Винни-Пух вбивает в программу с клавиатуры.
+# В ответе напишите “Парам пам-пам”, если с ритмом все в порядке
+# и “Пам парам”, если с ритмом все не в порядке
+
+vinny_song = input('Спойте песенку винни: ')
+vow_ru = "аяеёуюоэиы"
+
+
+def parsed_song(data: str):
+    words = data.split()
+    if len(words) <= 1:
+        print('Wrong song')
+        return None
+    else:
+        return words
+
+
+def is_rhythmic(song: list[str] | None, vwls: str):
+    check = False
+    if song:
+        bar = [wd.count(char) for wd in song for char in vwls if char in wd]
+        if len(set(bar)) == 1:
+            check = True
+    return check
+
+
+if is_rhythmic(parsed_song(vinny_song), vow_ru):
+    print('Парам пам-пам')
+else:
+    print('Пам парам')
---
+++ Homework_7/2.py
@@ -0,0 +1,13 @@
+# Напишите функцию print_operation_table(operation, num_rows=6, num_columns=6),
+# которая принимает в качестве аргумента функцию,
+# вычисляющую элемент по номеру строки и столбца.
+# Аргументы num_rows и num_columns указывают число строк и столбцов таблицы,
+# которые должны быть распечатаны.
+# Нумерация строк и столбцов идет с единицы (подумайте, почему не с нуля).
+
+def print_operation_table(func, row=6, column=6):
+    pass
+
+
+print_operation_table(lambda x, y: x * y)
+
